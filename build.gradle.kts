82467e43cc7ebb46b193caab0aec68db896390e7
yewton <yewton@gmail.com>
2022-11-28 10:02:17 +0900

    fix: IDEA 上のエラー表示に暫定対応する
    ルートの build.gradle.kts の Version Catalog 定義の libs を参照すると
    エラーになる件

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once https://github.com/gradle/gradle/issues/22797 is fixed
 plugins {
     alias(libs.plugins.versions)
     alias(libs.plugins.version.catalog.update)
