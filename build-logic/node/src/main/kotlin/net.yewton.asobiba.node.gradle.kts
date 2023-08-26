5de632f9e8aeea2fe467d354e826e5b5e609591f
yewton <yewton@gmail.com>
2023-08-26 22:27:39 +0900

    🆙 node, npm 最新化

--- build-logic/node/src/main/kotlin/net.yewton.asobiba.node.gradle.kts
+++ build-logic/node/src/main/kotlin/net.yewton.asobiba.node.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 
 configure<NodeExtension> {
     download.set(true)
-    version.set("18.16.0")
-    npmVersion.set("9.6.7")
+    version.set("18.17.1")
+    npmVersion.set("9.8.1")
 }
 
 tasks.npmInstall {
