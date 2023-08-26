606060d62a578006e29caf446b2bb8170af02113
yewton <yewton@gmail.com>
2023-08-26 22:56:15 +0900

    ✨ npm-check-updates を実行するタスクを追加する

--- build-logic/node/src/main/kotlin/net.yewton.asobiba.node.gradle.kts
+++ build-logic/node/src/main/kotlin/net.yewton.asobiba.node.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.gradle.node.NodeExtension
+import com.github.gradle.node.npm.task.NpxTask
 
 plugins {
     id("com.github.node-gradle.node")
@@ -16,4 +17,16 @@ tasks.npmInstall {
         // https://github.com/node-gradle/gradle-node-plugin/blob/master/docs/faq.md#how-do-i-ignore-some-files-of-the-node_modules-directory-that-are-modified-by-the-build-and-prevent-tasks-from-being-up-to-date-
         exclude("@esbuild")
     }
+}
+
+fun registeringNpmCheckUpdateTask(customizer: NpxTask.() -> Unit) = tasks.registering(NpxTask::class) {
+    command.set("npm-check-updates")
+    args.set(listOf("--target", "minor"))
+    customizer(this)
+}
+
+val npmCheckUpdate by registeringNpmCheckUpdateTask { }
+
+val npmUpgrade by registeringNpmCheckUpdateTask {
+    args.add("--upgrade")
 }
