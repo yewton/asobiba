a8be48c29d86e465ef97aca12b8c663bb99f488e
yewton <yewton@gmail.com>
2023-09-07 23:26:18 +0900

    (challenge) ♻ より Streamy な感じに

--- osunaba/challenge/src/main/java/net/yewton/asobiba/challenge/ch02_math/exercise/Ex08_Combinatorics.java
+++ osunaba/challenge/src/main/java/net/yewton/asobiba/challenge/ch02_math/exercise/Ex08_Combinatorics.java
@@ -1,14 +1,20 @@
 package net.yewton.asobiba.challenge.ch02_math.exercise;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.jetbrains.annotations.NotNull;
 
 public class Ex08_Combinatorics {
 
   public record ABC(int a, int b, int c) implements Comparable<ABC> {
+
+    public boolean isValid() {
+      return Math.pow(a(), 2) + Math.pow(b(), 2) == Math.pow(c(), 2);
+    }
+
     @Override
     public int compareTo(@NotNull ABC that) {
       var ac = Integer.compare(a, that.a());
@@ -25,20 +31,11 @@ public class Ex08_Combinatorics {
 
   /** a^2 + b^2 = c^2 を満たす a, b, c の組を見つける ( それぞれ 1 以上 100 未満の値まで )。 */
   public Set<ABC> findABCs() {
-    final Set<ABC> result = new HashSet<>();
-    IntStream.range(1, 100)
-        .forEach(
-            a ->
-                IntStream.range(1, 100)
-                    .forEach(
-                        b ->
-                            IntStream.range(1, 100)
-                                .forEach(
-                                    c -> {
-                                      if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
-                                        result.add(new ABC(a, b, c));
-                                      }
-                                    })));
-    return Collections.unmodifiableSet(result);
+    final Supplier<Stream<Integer>> newRange = () -> IntStream.range(1, 100).boxed();
+    return newRange
+        .get()
+        .flatMap(a -> newRange.get().flatMap(b -> newRange.get().map(c -> new ABC(a, b, c))))
+        .filter(ABC::isValid)
+        .collect(Collectors.toSet());
   }
 }
