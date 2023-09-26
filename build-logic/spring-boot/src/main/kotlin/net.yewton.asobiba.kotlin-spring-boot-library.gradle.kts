3962c850eddaf6efa3b5b4fa7de1b80bd61cbbc5
yewton <yewton@gmail.com>
2023-09-26 21:23:19 +0900

    📝(build-logic) 微妙なコメントを削除する
    Gradle の issue 14437 によると、precompiled scripts 内で apply false する意味が無いので、
    本来エラーとすべき挙動ではある模様。

--- build-logic/spring-boot/src/main/kotlin/net.yewton.asobiba.kotlin-spring-boot-library.gradle.kts
+++ build-logic/spring-boot/src/main/kotlin/net.yewton.asobiba.kotlin-spring-boot-library.gradle.kts
@@ -6,9 +6,6 @@ plugins {
     id("net.yewton.asobiba.kotlin-spring-boot-dependency")
     kotlin
     kotlin("plugin.spring")
-    // org.springframework.boot は dependencyManagement で参照する為だけに必要で、
-    // plugin としての apply は不要。クラスパスへの依存は build.gradle.kts で宣言済み。
-    // なお、ここで apply false で宣言していても何故か bootJar などのタスクが作られてしまう…。
 }
 
 tasks.withType<KotlinCompile>().configureEach {
