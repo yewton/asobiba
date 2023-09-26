54d481dfd614746129d72cb42ab11fadab7e4214
yewton <yewton@gmail.com>
2023-09-26 22:26:38 +0900

    🚿(platforms) kotlin BOM で定義される依存関係の宣言は不要

--- platforms/plugins-platform/build.gradle.kts
+++ platforms/plugins-platform/build.gradle.kts
@@ -10,10 +10,8 @@ dependencies {
     api(platform(libs.kotlin.bom))
     constraints {
         // TODO https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto.build.generate-git-info
-        api(libs.kotlin.jvm.gradle.plugin)
         api(libs.spotless.plugin)
         api(libs.detekt.plugin)
-        api(libs.kotlin.allopen)
         api(libs.spring.boot.plugin)
         api(libs.spring.dependency.management.plugin)
         api(libs.node.plugin)
