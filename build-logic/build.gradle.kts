96115f8b26b6da24eaa96fe4664d972879aefe68
yewton <yewton@gmail.com>
2023-09-26 22:15:09 +0900

    ✨(build-logic)大概の問題は clean して reload すれば直るので便利タスクを追加する

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,3 +1,14 @@
 allprojects {
     group = "net.yewton.asobiba.build-logic"
 }
+
+// https://discuss.gradle.org/t/gradle-clean-all-projects/10618/3
+val cleanAll by tasks.registering {
+    group = "build"
+}
+
+subprojects {
+    cleanAll {
+        dependsOn(tasks.matching { it.name == "clean" })
+    }
+}
