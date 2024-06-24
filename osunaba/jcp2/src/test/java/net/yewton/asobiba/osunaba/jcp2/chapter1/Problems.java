e593897f935cba5373e66da505824066009b7b87
yewton <yewton@gmail.com>
2024-06-24 14:35:56 +0900

    見た目整理

--- osunaba/jcp2/src/test/java/net/yewton/asobiba/osunaba/jcp2/chapter1/Problems.java
+++ osunaba/jcp2/src/test/java/net/yewton/asobiba/osunaba/jcp2/chapter1/Problems.java
@@ -10,7 +10,6 @@ import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 import org.assertj.core.api.WithAssertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -26,33 +25,34 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
 
-@SuppressWarnings("NewClassNamingConvention")
+@SuppressWarnings({"NewClassNamingConvention", "NonAsciiCharacters"})
 public class Problems implements WithAssertions {
-  @Test
-  @DisplayName("複数行に渡る文字列をつくる")
-  void p01() {
-    var joinStyle = String.join(
-        "\n",
-        "UPDATE \"public\".\"office\"",
-        "SET (\"address_first\", \"address_second\", \"phone\") =",
-        "  (SELECT \"public\".\"employee\".\"first_name\",",
-        "          \"public\".\"employee\".\"last_name\", ?",
-        "   FROM \"public\".\"employee\"",
-        "   WHERE \"public\".\"employee\".\"job_title\" = ?;");
-    var textBlockStyle =
-        """
-           UPDATE "public"."office"
-           SET ("address_first", "address_second", "phone") =
-             (SELECT "public"."employee"."first_name",
-                     "public"."employee"."last_name", ?
-              FROM "public"."employee"
-              WHERE "public"."employee"."job_title" = ?;""";
-    assertThat(textBlockStyle).isEqualTo(joinStyle);
+  @Nested
+  class P01_複数行に渡る文字列 {
+    @Test
+    void case01_同じSQL文字列をテキストブロック有無で書き比べてみる() {
+      var joinStyle = String.join(
+          "\n",
+          "UPDATE \"public\".\"office\"",
+          "SET (\"address_first\", \"address_second\", \"phone\") =",
+          "  (SELECT \"public\".\"employee\".\"first_name\",",
+          "          \"public\".\"employee\".\"last_name\", ?",
+          "   FROM \"public\".\"employee\"",
+          "   WHERE \"public\".\"employee\".\"job_title\" = ?;");
+      var textBlockStyle =
+          """
+             UPDATE "public"."office"
+             SET ("address_first", "address_second", "phone") =
+               (SELECT "public"."employee"."first_name",
+                       "public"."employee"."last_name", ?
+                FROM "public"."employee"
+                WHERE "public"."employee"."job_title" = ?;""";
+      assertThat(textBlockStyle).isEqualTo(joinStyle);
+    }
   }
 
   @Nested
-  @DisplayName("テキストブロックのデリミタの振る舞いについて")
-  class P02 {
+  class P02_テキストブロックのデリミタの振る舞いについて {
 
     String expected =
         """
@@ -64,8 +64,7 @@ public class Problems implements WithAssertions {
                          WHERE "public"."employee"."job_title" = ?)""";
 
     @Test
-    @DisplayName("インデントサイズは任意")
-    void p02_1() {
+    void case01_インデントサイズは任意() {
       var actual =
           """
           UPDATE "public"."office"
@@ -79,8 +78,7 @@ public class Problems implements WithAssertions {
 
     @SuppressWarnings("TrailingWhitespacesInTextBlock")
     @Test
-    @DisplayName("デリミタが右にずれていても影響はない")
-    void p02_2() {
+    void case02_デリミタが右にずれていても影響はない() {
       var actual =
           """
   UPDATE "public"."office"
@@ -93,8 +91,7 @@ public class Problems implements WithAssertions {
     }
 
     @Test
-    @DisplayName("終端デリミタを単独行に配置すると終端に改行が入る")
-    void p02_3() {
+    void case03_終端デリミタを単独行に配置すると終端に改行が入る() {
       var actual =
           """
   UPDATE "public"."office"
@@ -108,8 +105,7 @@ public class Problems implements WithAssertions {
     }
 
     @Test
-    @DisplayName("終端デリミタを単独行に左寄せで配置すると、文字列にインデントが入る")
-    void p02_4() {
+    void case04_終端デリミタを単独行に左寄せで配置すると文字列にインデントが入る() {
       var actual =
           """
   UPDATE "public"."office"
@@ -123,9 +119,8 @@ public class Problems implements WithAssertions {
     }
   }
 
-  @DisplayName("テキストブロックのインデントについて")
   @Nested
-  class P03 {
+  class P03_テキストブロックのインデントについて {
     String expected =
         """
               I would want to establish strength; root-like,
@@ -135,8 +130,7 @@ public class Problems implements WithAssertions {
               Prove I’m the poet of each line of prose.""";
 
     @Test
-    @DisplayName("インデントさせつつ改行を抑制できる")
-    void p03_1() {
+    void case01_インデントさせつつ改行を抑制できる() {
       var actual =
           """
               I would want to establish strength; root-like,
@@ -152,11 +146,9 @@ public class Problems implements WithAssertions {
 
   @SuppressWarnings("TextBlockMigration")
   @Nested
-  @DisplayName("可読性を上げる為にテキストブロックを利用するケース")
-  class P05 {
+  class P05_可読性を上げる為にテキストブロックを利用するケース {
     @Test
-    @DisplayName("先頭の空白が無い場合")
-    void p05_1() {
+    void case01_先頭の空白が無い場合() {
       var actual =
           """
              SELECT "public"."employee"."first_name" \
@@ -169,8 +161,7 @@ public class Problems implements WithAssertions {
     }
 
     @Test
-    @DisplayName("先頭に空白を含める場合")
-    void p05_2() {
+    void case02_先頭に空白を含める場合() {
       var actual =
           """
   UPDATE "public"."office" \
@@ -192,8 +183,7 @@ public class Problems implements WithAssertions {
     }
 
     @Test
-    @DisplayName("行末の空白を保持する場合")
-    void p05_3() {
+    void case03_行末の空白を保持する場合() {
       var actual =
           """
                  An old silent pond...\s\s\s
@@ -209,8 +199,7 @@ public class Problems implements WithAssertions {
     }
 
     @Test
-    @DisplayName("行末の空白を保持する場合 その2")
-    void p05_4() {
+    void case04_行末の空白を保持する場合_その2() {
       var actual =
           """
                  An old silent pond...  \s
@@ -227,135 +216,19 @@ public class Problems implements WithAssertions {
   }
 
   @Nested
-  @DisplayName("プログラムでエスケープシーケンスを解釈する")
-  class P07 {
+  class P07_プログラムでエスケープシーケンスを解釈する {
     @Test
-    @DisplayName("translateEscapes")
-    void p07_1() {
+    void case01_translateEscapes() {
       String newline = "\\n".translateEscapes();
       assertThat(newline).isEqualTo("\n");
     }
   }
 
-  @BenchmarkMode(Mode.AverageTime)
-  @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  @SuppressWarnings({"unused", "SameParameterValue"})
-  public static class P08Bench {
-    @Benchmark
-    public void benchConcat(Blackhole bh) {
-      bh.consume(concat("Jo", "Kym"));
-    }
-
-    @Benchmark
-    public void benchStringBuilder(Blackhole bh) {
-      bh.consume(stringBuilder("Jo", "Kym"));
-    }
-
-    @Benchmark
-    public void benchMessageFormat(Blackhole bh) {
-      bh.consume(messageFormat("Jo", "Kym"));
-    }
-
-    @Benchmark
-    public void benchStringFormat(Blackhole bh) {
-      bh.consume(stringFormat("Jo", "Kym"));
-    }
-
-    @Benchmark
-    public void benchStringFormatted(Blackhole bh) {
-      bh.consume(stringFormatted("Jo", "Kym"));
-    }
-
-    String concat(String fn, String ln) {
-      return """
-             <user>
-                <firstName>
-             """
-          + fn.indent(4)
-          + """
-                 </firstName>
-                 <lastName>
-              """
-          + ln.indent(4)
-          + """
-                 </lastName>
-              </user>
-              """;
-    }
-
-    @SuppressWarnings("StringBufferReplaceableByString")
-    String stringBuilder(String fn, String ln) {
-      StringBuilder sbJson = new StringBuilder();
-      sbJson
-          .append("""
-             <user>
-                <firstName>
-              """)
-          .append(fn.indent(4))
-          .append("""
-                </firstName>
-                <lastName>
-             """)
-          .append(ln.indent(4))
-          .append("""
-                </lastName>
-             </user>
-             """);
-      return sbJson.toString();
-    }
-
-    String messageFormat(String fn, String ln) {
-      return MessageFormat.format(
-          """
-                            <user>
-                                <firstName>
-                                 {0}
-                                </firstName>
-                                <lastName>
-                                 {1}
-                                </lastName>
-                            </user>
-                            """,
-          fn, ln);
-    }
-
-    String stringFormat(String fn, String ln) {
-      return String.format(
-          """
-                           <user>
-                               <firstName>
-                                %s
-                               </firstName>
-                               <lastName>
-                                %s
-                               </lastName>
-                           </user>
-                           """,
-          fn, ln);
-    }
-
-    String stringFormatted(String fn, String ln) {
-      return """
-             <user>
-                 <firstName>
-                  %s
-                 </firstName>
-                 <lastName>
-                  %s
-                 </lastName>
-             </user>
-             """
-          .formatted(fn, ln);
-    }
-  }
-
   @Nested
-  @DisplayName("変数や式を含むテキストブロック")
-  public class P08 {
+  public class P08_変数や式を含むテキストブロック {
 
     @Test
-    @DisplayName("様々な方法でベンチマーク")
-    void runBench() throws RunnerException {
+    void 様々な方法でベンチマーク() throws RunnerException {
       var opt = new OptionsBuilder()
           .include(P08Bench.class.getSimpleName())
           .forks(1)
@@ -366,56 +239,170 @@ public class Problems implements WithAssertions {
           .build();
       new Runner(opt).run();
     }
-  }
 
-  public static class IsomorphicChecker {
-    public boolean perform(String s1, String s2) {
-      if (Objects.isNull(s1) || Objects.isNull(s2) || s1.length() != s2.length()) {
-        return false;
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @SuppressWarnings({"unused", "SameParameterValue"})
+    public static class P08Bench {
+      @Benchmark
+      public void benchConcat(Blackhole bh) {
+        bh.consume(concat("Jo", "Kym"));
       }
-      Map<Character, Character> map = new HashMap<>();
-      for (int i = 0; i < s1.length(); i++) {
-        char chs1 = s1.charAt(i);
-        char chs2 = s2.charAt(i);
-        if (map.containsKey(chs1)) {
-          if (map.get(chs1) != chs2) {
-            return false;
-          }
-        } else {
-          if (map.containsValue(chs2)) {
-            return false;
-          }
-          map.put(chs1, chs2);
-        }
+
+      @Benchmark
+      public void benchStringBuilder(Blackhole bh) {
+        bh.consume(stringBuilder("Jo", "Kym"));
       }
-      return true;
+
+      @Benchmark
+      public void benchMessageFormat(Blackhole bh) {
+        bh.consume(messageFormat("Jo", "Kym"));
+      }
+
+      @Benchmark
+      public void benchStringFormat(Blackhole bh) {
+        bh.consume(stringFormat("Jo", "Kym"));
+      }
+
+      @Benchmark
+      public void benchStringFormatted(Blackhole bh) {
+        bh.consume(stringFormatted("Jo", "Kym"));
+      }
+
+      String concat(String fn, String ln) {
+        return """
+             <user>
+                <firstName>
+             """
+            + fn.indent(4)
+            + """
+                 </firstName>
+                 <lastName>
+              """
+            + ln.indent(4)
+            + """
+                 </lastName>
+              </user>
+              """;
+      }
+
+      @SuppressWarnings("StringBufferReplaceableByString")
+      String stringBuilder(String fn, String ln) {
+        StringBuilder sbJson = new StringBuilder();
+        sbJson
+            .append("""
+             <user>
+                <firstName>
+             """)
+            .append(fn.indent(4))
+            .append("""
+                </firstName>
+                <lastName>
+             """)
+            .append(ln.indent(4))
+            .append("""
+                </lastName>
+             </user>
+             """);
+        return sbJson.toString();
+      }
+
+      String messageFormat(String fn, String ln) {
+        return MessageFormat.format(
+            """
+                              <user>
+                                  <firstName>
+                                   {0}
+                                  </firstName>
+                                  <lastName>
+                                   {1}
+                                  </lastName>
+                              </user>
+                              """,
+            fn, ln);
+      }
+
+      String stringFormat(String fn, String ln) {
+        return String.format(
+            """
+                             <user>
+                                 <firstName>
+                                  %s
+                                 </firstName>
+                                 <lastName>
+                                  %s
+                                 </lastName>
+                             </user>
+                             """,
+            fn, ln);
+      }
+
+      String stringFormatted(String fn, String ln) {
+        return """
+             <user>
+                 <firstName>
+                  %s
+                 </firstName>
+                 <lastName>
+                  %s
+                 </lastName>
+             </user>
+             """
+            .formatted(fn, ln);
+      }
     }
   }
 
-  @ParameterizedTest(name = "[{index}] ''{0}'' と ''{1}'' は {2}")
-  @DisplayName("文字列の同型判定")
-  @MethodSource
-  void p12(String s1, String s2, boolean expected) {
-    assertThat(new IsomorphicChecker().perform(s1, s2)).isEqualTo(expected);
-  }
+  @Nested
+  class P12_文字列の同型判定 {
+    static class IsomorphicChecker {
+      public boolean perform(String s1, String s2) {
+        if (Objects.isNull(s1) || Objects.isNull(s2) || s1.length() != s2.length()) {
+          return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+          char chs1 = s1.charAt(i);
+          char chs2 = s2.charAt(i);
+          if (map.containsKey(chs1)) {
+            if (map.get(chs1) != chs2) {
+              return false;
+            }
+          } else {
+            if (map.containsValue(chs2)) {
+              return false;
+            }
+            map.put(chs1, chs2);
+          }
+        }
+        return true;
+      }
+    }
 
-  static Stream<Arguments> p12() {
-    var truthy = named("同型である", true);
-    var falsy = named("同型でない", false);
-    return Stream.of(
-        arguments("abbcdd", "qwwerr", truthy),
-        arguments("aab", "que", falsy),
-        arguments(
-            """
-                   abbcdd
-                     baaaad
-                   ccddaa
-                   """,
-            """
-                   qwwerr
-                     wqqqqr
-                   eerrqq
-                   """,
-            truthy));
+    @ParameterizedTest(name = "[{index}] ''{0}'' と ''{1}'' は {2}")
+    @MethodSource
+    void p12(String s1, String s2, boolean expected) {
+      assertThat(new IsomorphicChecker().perform(s1, s2)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> p12() {
+      var truthy = named("同型である", true);
+      var falsy = named("同型でない", false);
+      return Stream.of(
+          arguments("abbcdd", "qwwerr", truthy),
+          arguments("aab", "que", falsy),
+          arguments(
+              """
+                     abbcdd
+                       baaaad
+                     ccddaa
+                     """,
+              """
+                     qwwerr
+                       wqqqqr
+                     eerrqq
+                     """,
+              truthy));
+    }
   }
 }
